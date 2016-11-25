3ef067fa3e5fb142dd03d5be4662b9a8c4dd4198
王海峰 <wangcityboy@163.com>
2016-11-25 17:21:41 +0800

    update

--- photo_detail.php
+++ photo_detail.php
@@ -128,10 +128,10 @@ if (isset($_GET['id'])) {
 		<h1 class="bw_bc_title"><?php echo $_html['name']?></h1>
                       <div class="titleTopBlock">
                      		 <span class="leftBlock">
-                                发表于：<?php echo $_html['date']?> | 上传者：<?php echo $_html['username']?>
+                                发表于:&nbsp; <?php echo $_html['date']?> | 上传者:&nbsp; <?php echo $_html['username']?>
                             </span>
                             <span class="rightBlock">
-                            	浏览量:(<strong><?php echo $_html['readcount'];?></strong>) | 评论量:(<strong><?php echo $_html['commendcount'];?></strong>)
+                            	浏览量:&nbsp;[<strong><?php echo $_html['readcount'];?></strong>] | 评论量:&nbsp;[<strong><?php echo $_html['commendcount'];?></strong>]
                             </span>
                       </div>
 
